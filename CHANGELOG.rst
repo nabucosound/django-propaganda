aa402dfc5eccbd00744623eef844acc597834e9d
Hector Garcia <hector@nomadblue.com>
2015-03-21 21:35:55 -0300

    Fix package files and configurations

---
+++ CHANGELOG.rst
@@ -0,0 +1,8 @@
+=========
+CHANGELOG
+=========
+
+Version 1.1.2
+=============
+
+* First release of package to include this CHANGELOG.
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from distutils.core import setup
 
+package_dirs = ('base_accounts',)
+
 
 def fullsplit(path, result=None):
     """
@@ -10,48 +12,50 @@ def fullsplit(path, result=None):
     if result is None:
         result = []
     head, tail = os.path.split(path)
-    if head == "":
+    if head == '':
         return [tail] + result
     if head == path:
         return result
     return fullsplit(head, [tail] + result)
 
-package_dir = "propaganda"
-packages = []
-for dirpath, dirnames, filenames in os.walk(package_dir):
-    # ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith("."):
-            del dirnames[i]
-    if "__init__.py" in filenames:
-        packages.append(".".join(fullsplit(dirpath)))
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+for package_dir in package_dirs:
+    for dirpath, dirnames, filenames in os.walk(package_dir):
+        # Ignore dirnames that start with '.'
+        for i, dirname in enumerate(dirnames):
+            if dirname.startswith('.'):
+                del dirnames[i]
+        if '__init__.py' in filenames:
+            packages.append('.'.join(fullsplit(dirpath)))
+        elif filenames:
+            data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
-version = __import__('propaganda').VERSION
+version_num = __import__('propaganda').__version__
 
 setup(
     name='django-propaganda',
-    version=version,
-    url='http://github.com/nabucosound/django-propaganda',
-    download_url="https://github.com/nabucosound/django-propaganda/zipball/v%s" % version,
-    author='Hector Garcia',
-    author_email='hector@nomadblue.com',
+    version=version_num,
     description='Django app to manage your daily newsletter delivery.',
     long_description=open('README.rst').read(),
+    author='Hector Garcia',
+    author_email='hector@nomadblue.com',
+    url='https://github.com/nabucosound/django-propaganda',
+    download_url="https://github.com/nabucosound/django-propaganda/archive/v%s.zip" % version_num,
     packages=packages,
-    classifiers = [
+    package_data={'base_accounts': ['templates/base_accounts/*']},  # Include templates and statics here
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
-        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
-   ],
+    ]
 )
